30d0b6a9e8f9c084ed4fd2b8595dc2b682dfd26b
napalm684 <svause@gmail.com>
2022-05-20 21:09:47 -0400

    DI and mocking setup

--- dependency-injection/greet_test.go
+++ dependency-injection/greet_test.go
@@ -1,3 +1,18 @@
 package main
 
-//https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/dependency-injection
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Chris")
+
+	got := buffer.String()
+	want := "Hello, Chris"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
